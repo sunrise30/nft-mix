88f93d13e26d0619a9e825b2fba3e968c4db2037
sunrise30 <sunrise.30.yavis@gmail.com>
2021-09-20 21:59:50 -0500

    Install dependencies

---
+++ README.md
@@ -0,0 +1,6 @@
+# nft-mix
+This is a repo to work with and use NFTs smart contracts in a python environment, using the Chainlin-mix as a starting point
+### Compile
+```
+brownie compile
+```
---
+++ contracts/AdvancedCollectible.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
+
+contract AdvancedCollectible is ERC721, VRFConsumerBase {
+
+}
